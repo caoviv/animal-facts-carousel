6ab00fb95316d92e549965acdd6f90a3fa4affd7
VivianC <viviancao58@gmail.com>
2022-11-10 18:12:27 +1100

    change reviews to facts

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 // local reviews data
-const reviews = [
+const facts = [
     {
       id: 1,
-      name: "susan smith",
-      job: "web developer",
+      animal: "susan smith",
+      type: "web developer",
       img:
         "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
-      text:
+      fact:
         "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
       id: 2,
-      name: "anna johnson",
-      job: "web designer",
+      animal: "anna johnson",
+      type: "web designer",
       img:
         "https://images.unsplash.com/photo-1521146764736-56c929d59c83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      fact:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee eanimall pin fashion axe.photo booth jean shorts artisan narwhal.",
     },
     {
       id: 3,
-      name: "peter jones",
-      job: "intern",
+      animal: "peter jones",
+      type: "intern",
       img:
         "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
-      text:
+      fact:
         "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
     },
     {
       id: 4,
-      name: "bill anderson",
-      job: "the boss",
+      animal: "bill anderson",
+      type: "the boss",
       img:
         "https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVyc29uJTIwcG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
-      text:
+      fact:
         "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
     },
 ];
 
 //variables - select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
+const img = document.getElementById('img');
+const animal = document.getElementById('animal');
+const type = document.getElementById('type');
+const fact = document.getElementById('fact');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
@@ -53,45 +53,45 @@ let currentItem = 1;
 
 // event listeners
 // load initial item 
-window.addEventListener('DOMContentLoaded', showReview(currentItem));
+window.addEventListener('DOMContentLoaded', showFact);
 // show next review 
-nextBtn.addEventListener('click', nextReview);
+nextBtn.addEventListener('click', nextFact);
 // show previous review 
-prevBtn.addEventListener('click', prevReview);
+prevBtn.addEventListener('click', prevFact);
 // show random review 
-randomBtn.addEventListener('click', randomReview);
+randomBtn.addEventListener('click', randomFact);
 
 // functions 
 // show review based on item 
-function showReview(person) {
-  const item = reviews[person];
+function showFact() {
+  const item = facts[currentItem];
   img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+  animal.textContent = item.animal;
+  type.textContent = item.type;
+  fact.textContent = item.fact;
 }
 
 // move to next review in array 
-function nextReview () {
+function nextFact () {
   currentItem ++;
-  if (currentItem > reviews.length - 1) {
+  if (currentItem > facts.length - 1) {
     currentItem = 0;
   }
-  showReview(currentItem);
+  showFact(currentItem);
 }
 
 // move to previous review in array 
-function prevReview () {
+function prevFact () {
   currentItem --;
   if (currentItem < 0) {
-    currentItem = reviews.length - 1;
+    currentItem = facts.length - 1;
   }
-  showReview(currentItem);
+  showFact(currentItem);
 }
 
 // show random review in array 
-function randomReview () {
-  currentItem = Math.floor(Math.random() * reviews.length)
+function randomFact () {
+  currentItem = Math.floor(Math.random() * facts.length)
 
-  showReview(currentItem);
+  showFact(currentItem);
 }
